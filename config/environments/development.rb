625911f91662c43e7a6144f5d8b1d6148e884b2c
K31THSTONE <goliff@ycp.edu>
2017-08-05 15:41:07 +0000

    Added Action Mailer Perform Deliveries Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   config.action_mailer.default_url_options = { :host => '69.251.75.122', :port => 3000 }
-
+  config.action_mailer.perform_deliveries = true
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
