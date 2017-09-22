a68fb978e4ba5a97356a9d2118b71197eba6f79c
cdierdorff <cdierdorff@ycp.edu>
2017-09-22 16:02:30 -0400

    scores controller updates

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,29 @@
 class ScoresController < ApplicationController
+    def index
+    @scores = Score.all
+  end
+  
+  def new
+    @score = Score.new
+  end
+  
+  def show
+    @scores = Score.find(params[:id])
+  end
+  
+  def create
+    @scores = Quiz.first.answers.build(answer_params)
+    if @score.save
+      redirect_to root_url
+    else
+      render static_pages/home
+    end
+  end  
+  
+  private
+    # Never trust parameters from the scary internet, only allow the white list 
+through.
+    def answer_params
+      params.require(:answer).permit(:content, :correct)
+    end
 end
